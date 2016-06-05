957309603b1cd1c76fa40580a1585755845bb172
Alejandro Zorita <alzort@gmail.com>
2016-06-05 23:58:03 +0200

    Retocamos vistas y css

--- resources/assets/views/partials/menu.blade.php
+++
@@ -1,7 +0,0 @@
-<ul class="nav navbar-nav">
-    @foreach ($items as $route => $text)
-    <li role="presentation" {!! Html::classes(['active' => Route::is($route)]) !!}>
-        <a href="{{ route($route) }}">{{ $text }}</a>
-    </li>
-    @endforeach
-</ul>
--- resources/assets/views/tickets/partials/status.blade.php
+++
@@ -1,3 +0,0 @@
-<span{!! Html::classes(['label label-info absolute', 'highlight' => $ticket->open]) !!}>
-    {{ trans('tickets.status.' . $ticket->status) }}
-</span>
