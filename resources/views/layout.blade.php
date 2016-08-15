9116fd91167deec5e2b2ef9795a31cdf37b90382
Alejandro Zorita <alzort@gmail.com>
2016-08-15 19:15:49 +0200

    Cambio logo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -34,7 +34,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="http://bytecode.es/" target="_blank">bytecode.me</a>
+                    <a class="" href="http://bytecode.es/" target="_blank">bytecode.me</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
